ccf109d6f8b8d51209b44f816cd1807f5bbe29c6
user <user@WIN10-AUG2016>
2016-08-27 19:39:18 -0700

    Criado controlador de api ok.

---
+++ Livroteca/Controllers/LivroController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Livroteca.Models;
+
+namespace Livroteca.Controllers
+{
+    public class LivroController : ApiController
+    {
+        private LivroDb db = new LivroDb();
+
+        // GET: api/Livro
+        public IQueryable<Livro> GetLivros()
+        {
+            return db.Livros;
+        }
+
+        // GET: api/Livro/5
+        [ResponseType(typeof(Livro))]
+        public IHttpActionResult GetLivro(int id)
+        {
+            Livro livro = db.Livros.Find(id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(livro);
+        }
+
+        // PUT: api/Livro/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLivro(int id, Livro livro)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != livro.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(livro).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LivroExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Livro
+        [ResponseType(typeof(Livro))]
+        public IHttpActionResult PostLivro(Livro livro)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Livros.Add(livro);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = livro.Id }, livro);
+        }
+
+        // DELETE: api/Livro/5
+        [ResponseType(typeof(Livro))]
+        public IHttpActionResult DeleteLivro(int id)
+        {
+            Livro livro = db.Livros.Find(id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+
+            db.Livros.Remove(livro);
+            db.SaveChanges();
+
+            return Ok(livro);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LivroExists(int id)
+        {
+            return db.Livros.Count(e => e.Id == id) > 0;
+        }
+    }
+}
