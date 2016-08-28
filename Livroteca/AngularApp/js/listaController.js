be9ac8cb8a3f0080b568dd135e72182f59273c7c
user <user@WIN10-AUG2016>
2016-08-27 20:04:26 -0700

    Utilizando o angular JS para fazer requisicoes na api web

---
+++ Livroteca/AngularApp/js/listaController.js
@@ -0,0 +1,13 @@
+﻿(function (app) {
+
+    var listaController = function ($scope, $http) {
+
+        $http.get("/api/livro")
+              .success(function (data) {
+                  $scope.livros = data;
+              });
+    };
+
+    app.controller("listaController", listaController)
+
+}(angular.module("livroteca", [])))
