72e6b6c47768ae2ac5361ccb7c50c7905c1cfcc0
user <user@WIN10-AUG2016>
2016-08-27 18:47:57 -0700

    Inicial Comit

---
+++ Livroteca/Models/Livro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Livroteca.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Anolancamento { get; set; }
+        public int NumeroDePaginas { get; set; }
+
+    }
+}
---
+++ Livroteca/Models/LivroDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Livroteca.Models
+{
+    public class LivroDb
+    {
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
